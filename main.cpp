a4889b041cce518140b41f340b38fc60181ef210
Jan M <jmarschatz@gmail.com>
2021-05-23 16:56:33 -0600

    Numeros aleatoreos
    Se creo una funcion la cual guarda numeros aleatorios, esta funcion se utilizara para cuando el usuario seleccione la cantidad de jugadores que quiera, los numeros aleatoreos seran las posiciones en donde se colocaran cada uno.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,44 @@
 #include "SFML/Graphics.hpp"
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 #include "Menu.h"
+
+using namespace std;
+using namespace sf;
+
+
 
 int main()
 {
+	int numeros_random[20];
+	int no_repetido;
+	srand(time(NULL));
+	int g;
+	for (int i = 0; i < 20; i++)
+	{
+		g = i;
+		no_repetido = 1 + rand() % (21 - 1);
+		for (int j = 0; j <= g; j++)
+		{
+			if (no_repetido == numeros_random[j]) {
+				g = 0;
+				i -= 1; 
+			}
+
+			while ((g == i) && (no_repetido != numeros_random[j]) && (j==i))
+			{
+				numeros_random[i] = no_repetido;
+				cout << numeros_random[i] << endl;
+			}
+		} 
+	}
+
 	sf::RenderWindow window(sf::VideoMode(900, 900), "SFML WORK!");
+	
+	sf::CircleShape balon(100);
+	balon.setFillColor(Color::Magenta);
 
 	Menu menu(window.getSize().x, window.getSize().y);
 
@@ -32,6 +66,9 @@ int main()
 					{
 					case 0:
 						std::cout << "Genetic Puzzle se esta abriendo" << std::endl;
+						//window.setVisible(false);
+						window.close();
+						//abrir();
 						break;
 					case 1:
 						std::cout << "BP Game se esta abriendo" << std::endl;
@@ -60,6 +97,45 @@ int main()
 
 		menu.draw(window);
 
+		window.draw(balon);
+
 		window.display();
 	}
+
+	sf::RenderWindow ventana(sf::VideoMode(1400, 800), "Lets play");
+	ventana.setFramerateLimit(60);
+
+
+	while (ventana.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (ventana.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case Event::Closed:
+				ventana.close();
+				break;
+
+			case Event::MouseButtonReleased:
+				cout << "Se preciono" << endl;
+
+				switch (event.key.code)
+				{
+				case Mouse::Left:
+					cout << "Se preciono la izquierda" << endl;
+					break;
+				}
+				break;
+			}
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed) {
+				ventana.close();
+			}
+		}
+		ventana.clear();
+		ventana.display();
+	}
 }
+
