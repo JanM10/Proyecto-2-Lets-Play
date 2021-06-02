7d3e1be0998bf1cd9fb7e4f9c3ed02d5ee40228a
Jan M <jmarschatz@gmail.com>
2021-06-01 22:46:45 -0600

    Documentacion interna

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ void numerosRandom(int matriz[11][21]);
 
 void imprimitMatriz(int matriz[11][21]);
 
+bool flag = true;//Para ver cual ventana se abre si el BP game o el Puzzle
 
 int cantObs = 9;//Cantidad de obstaculos
 
@@ -259,50 +260,18 @@ void aStarSearch(int matriz[11][21], const Pair& src, const Pair& dest)
 			}
 		}
 	}
-
-
 	printf("No se encontro la celda\n");
 }
 
 int main()
 {
-	sf::RenderWindow obstaculos(sf::VideoMode(800, 600), "Cauantos Obstaculos");
 
-	sf::String playerInput;
-	sf::Text playerText;
-
-	// run the program as long as the window is open
-	while (obstaculos.isOpen())
-	{
-		// check all the window's events that were triggered since the last iteration of the loop
-		sf::Event event;
-		while (obstaculos.pollEvent(event))
-		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
-				obstaculos.close();
-
-			if (event.type == sf::Event::TextEntered)
-			{
-				playerInput += event.text.unicode;
-				playerText.setString(playerInput);
-			}
-
-
-			//obstaculos.display();
-		}
-		
-		obstaculos.clear();
-		obstaculos.draw(playerText);
-		obstaculos.display();
-	}
 
 
 	RenderWindow window(sf::VideoMode(900, 900), "Let's Play!");
-
-
 	Menu menu(window.getSize().x, window.getSize().y);
 
+	//Este ciclo abre el menu principal
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -331,6 +300,8 @@ int main()
 						break;
 					case 1:
 						std::cout << "BP Game se esta abriendo" << std::endl;
+						flag = false;
+						window.close();
 						break;
 					case 2:
 						std::cout << "Abriendo las opciones" << std::endl;
@@ -359,126 +330,144 @@ int main()
 		window.display();
 	}
 
-	/////////////////////////////////////////////////////////////////////
-	sf::RenderWindow ventanaPrueba(sf::VideoMode(1400, 800), "My window");
-	const int chanchaDim = 60; //Dimensiones de la cancha
-	sf::RectangleShape cuadrados(sf::Vector2f(chanchaDim, chanchaDim));
-
-	const int tamanoCancha = 22;
-
-	vector<vector<RectangleShape>> cuadradosCancha;
-
-	cuadradosCancha.resize(tamanoCancha, vector<sf::RectangleShape>());
-
-	for (int x = 0; x < 11; x++)
+	//Si el flag es falso se abre la ventana del BP Game
+	if (flag == false)
 	{
-		cuadradosCancha[x].resize(tamanoCancha, sf::RectangleShape());
-		for (int y = 0; y < 21; y++)
+		sf::RenderWindow obstaculos(sf::VideoMode(400, 200), "Cauantos Obstaculos");
+		sf::String playerInput;
+		sf::Text playerText;
+
+		//El programa sigue corriendo siemrpre y cuando la ventana este abierta
+		while (obstaculos.isOpen())
 		{
-			cuadradosCancha[x][y].setSize(sf::Vector2f(chanchaDim, chanchaDim));
-			cuadradosCancha[x][y].setFillColor(Color::Green);
-			cuadradosCancha[x][y].setOutlineThickness(3.0f);
-			cuadradosCancha[x][y].setOutlineColor(Color::Black);
-			cuadradosCancha[x][y].setPosition((y + 1) * chanchaDim, (x + 1) * chanchaDim);
-		}
-	}
 
-	//0-8   0-18
-	//array<array<int, 21>, 11>grid{     
-	//	{{{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }},
-	//	{{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }},
-	//	{{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }},
-	//	{{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }},
-	//	{{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 }},
-	//	{{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 }},
-	//	{{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 }},
-	//	{{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }},
-	//	{{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }},
-	//	{{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }},
-	//	{{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }}} };
-
-	int cuadrosCancha2[11][21]{
-	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-	{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-	{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-	{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
-
-
-	numerosRandom(cuadrosCancha2);
-
-	Pair src(5, 10);
-
-	// Destination is the left-most top-most corner
-	Pair dest(5, 19);
-
-	aStarSearch(cuadrosCancha2, src, dest);
-
-	for (int x = 0; x < 11; x++)
-	{
-		for (int y = 0; y < 21; y++)
-		{
-			if (cuadrosCancha2[x][y] == 1)
+			//Revisa todos los eventos que se activaron desde la ultima iteracion 
+			sf::Event event;
+			while (obstaculos.pollEvent(event))
 			{
-				cuadradosCancha[x][y].setFillColor(Color::Blue);
-			}
-			else if (cuadrosCancha2[x][y] == 2) {
-				cuadradosCancha[x][y].setFillColor(Color::Red);
-			}
-			else if (cuadrosCancha2[x][y] == 3) {
-				cuadradosCancha[x][y].setFillColor(Color::Black);
-			}
-			else if (cuadrosCancha2[x][y] == 4){
-				cuadradosCancha[x][y].setFillColor(Color::White);
-			}
+				//Se cierra la ventana
+				if (event.type == sf::Event::Closed)
+					obstaculos.close();
 
-		}
-
-	}
-
-
-
-	// run the program as long as the window is open
-	while (ventanaPrueba.isOpen())
-	{
-		// check all the window's events that were triggered since the last iteration of the loop
-		sf::Event event;
-		while (ventanaPrueba.pollEvent(event))
-		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
-				ventanaPrueba.close();
-
-			ventanaPrueba.clear(sf::Color(255, 255, 255));
-
-			for (int x = 0; x < 11; x++)
-			{
-				for (int y = 0; y < 21; y++)
+				if (event.type == sf::Event::TextEntered)
 				{
-					ventanaPrueba.draw(cuadradosCancha[x][y]);
+					playerInput += event.text.unicode;
+					playerText.setString(playerInput);
 				}
 			}
-
-			ventanaPrueba.display();
+			obstaculos.clear(sf::Color::White);
+			obstaculos.draw(playerText);
+			obstaculos.display();
 		}
-	}
 
+		sf::RenderWindow ventanaPrueba(sf::VideoMode(1400, 800), "My window");
+		const int chanchaDim = 60; //Dimensiones de la cancha
+		sf::RectangleShape cuadrados(sf::Vector2f(chanchaDim, chanchaDim));
+
+		const int tamanoCancha = 22;//Cantidad maxima de cuadrados que puede haber por fila y columna
+
+		vector<vector<RectangleShape>> cuadradosCancha;//En ese vector se almacenan los cuadrados de la cancha
+
+		cuadradosCancha.resize(tamanoCancha, vector<sf::RectangleShape>());
+
+		//Este for rellena la el vector con los cuadrados que se van a mostrar en pantalla, junto con sus colores y rayas
+		for (int x = 0; x < 11; x++)
+		{
+			cuadradosCancha[x].resize(tamanoCancha, sf::RectangleShape());
+			for (int y = 0; y < 21; y++)
+			{
+				cuadradosCancha[x][y].setSize(sf::Vector2f(chanchaDim, chanchaDim));
+				cuadradosCancha[x][y].setFillColor(Color::Green);
+				cuadradosCancha[x][y].setOutlineThickness(3.0f);
+				cuadradosCancha[x][y].setOutlineColor(Color::Black);
+				cuadradosCancha[x][y].setPosition((y + 1) * chanchaDim, (x + 1) * chanchaDim);
+			}
+		}
+
+
+		int cuadrosCancha2[11][21]{
+		{ 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
+		{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+		{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+		{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+		{ 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 },
+		{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+		{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+		{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3 },
+		{ 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 } };
+
+
+		numerosRandom(cuadrosCancha2);
+
+		//Src(source) es el inicio del pathfinding
+		Pair src(5, 10);
+
+		//Dest muestra el destino del pathfinding
+		Pair dest(5, 19);
+
+		aStarSearch(cuadrosCancha2, src, dest);
+
+		for (int x = 0; x < 11; x++)
+		{
+			for (int y = 0; y < 21; y++)
+			{
+				if (cuadrosCancha2[x][y] == 1)
+				{
+					cuadradosCancha[x][y].setFillColor(Color::Blue);//El azul muestra los bordes del mapa 
+				}
+				else if (cuadrosCancha2[x][y] == 2) {
+					cuadradosCancha[x][y].setFillColor(Color::Red);//El rojo muestra las porterias
+				}
+				else if (cuadrosCancha2[x][y] == 3) {
+					cuadradosCancha[x][y].setFillColor(Color::Black);//El negro muestra los bordes del mapa
+				}
+				else if (cuadrosCancha2[x][y] == 4) {//El color blanco representa la ruta del pathfinding
+					cuadradosCancha[x][y].setFillColor(Color::White);
+				}
+
+			}
+
+		}
+
+		// run the program as long as the window is open
+		while (ventanaPrueba.isOpen())
+		{
+			// check all the window's events that were triggered since the last iteration of the loop
+			sf::Event event;
+			while (ventanaPrueba.pollEvent(event))
+			{
+				// "close requested" event: we close the window
+				if (event.type == sf::Event::Closed)
+					ventanaPrueba.close();
+
+				ventanaPrueba.clear(sf::Color(255, 255, 255));
+
+				for (int x = 0; x < 11; x++)
+				{
+					for (int y = 0; y < 21; y++)
+					{
+						ventanaPrueba.draw(cuadradosCancha[x][y]);
+					}
+				}
+
+				ventanaPrueba.display();
+			}
+		}
+	}else {
+	/*
+	* AQUI HAY QUE PONER EL CODIGO DEL PUZZLE GAME
+	*/
+	}
 }
 
 
-//1-9    1-10
+//Esta funciona elige las posiciones de los judagores o obstaculos aleatoriamente
 void numerosRandom(int matriz[11][21]) {
 	srand(time(NULL)); //Numeros aleatorios
 
-	//####################################################################
-	//Cambiar el nueve por el numero random que pida el profe de obstaculos
-	//####################################################################
+
 	int filas[9], columnas[9], num = 1;
 
 	for (int i = 9; i > abs(cantObs-9); i--)
@@ -487,9 +476,6 @@ void numerosRandom(int matriz[11][21]) {
 		columnas[i-1] = 0;
 	}
 
-	//####################################################################
-	//Cambiar el nueve por el numero random que pida el profe de obstaculos
-	//####################################################################
 	//Para que no se repitan los numeros de filas
 	for (int i = 0; i < cantObs; i++)
 	{
@@ -501,10 +487,6 @@ void numerosRandom(int matriz[11][21]) {
 		filas[i] = num;
 	}
 
-
-	//####################################################################
-	//Cambiar el nueve por el numero random que pida el profe de obstaculos
-	//####################################################################
 	//Para que no se repitan los numeros de columnas
 	for (int i = 0; i < cantObs; i++)
 	{
@@ -515,22 +497,18 @@ void numerosRandom(int matriz[11][21]) {
 		columnas[i] = num;
 	}
 
-
-	//####################################################################
-	//Cambiar el nueve por el numero random que pida el profe de obstaculos
-	//####################################################################
-	// 20-#columna
+	// 20-#columna para poder hacer la reflexion del otro lado de la matriz
 	for (int x = 0; x < 9; x++)
 	{
 		matriz[filas[x]][columnas[x]] = 1;
 		matriz[filas[x]][20-columnas[x]] = 1;
 	}
 
-
 	imprimitMatriz(matriz);
 
 }
 
+//Imprime los valores numericos de la matriz en consola
 void imprimitMatriz(int matriz[11][21])
 {
 	for (int x = 0; x < 11; x++)
